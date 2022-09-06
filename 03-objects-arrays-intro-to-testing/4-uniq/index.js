015a8271bc094cf86f5a4ea8229c434d36008ac1
QuebecNova <selllmenya@gmail.com>
2022-09-06 18:58:29 +0300

    uniq task done

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,12 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-
+export function uniq(arr = []) {
+  const uniqArr = [];
+  arr.forEach((value) => {
+    if (!uniqArr.includes(value)) {
+      uniqArr.push(value);
+    }
+  });
+  return uniqArr;
 }
