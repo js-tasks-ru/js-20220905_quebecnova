9d5bd069f519530626e770c2245f9027979a4084
QuebecNova <selllmenya@gmail.com>
2022-09-06 18:44:43 +0300

    trimSymbols task done

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) return string;
+  const charCounter = {};
+  let newString = "";
+  let i = 0;
+  for (const char of string) {
+    if (!charCounter[char]) {
+      charCounter[char] = 1;
+    } else {
+      charCounter[char] += 1;
+    }
 
+    if (charCounter[char] <= size) {
+      newString += char;
+    }
+
+    const nextChar = string[i + 1];
+    if (nextChar !== char) {
+      charCounter[char] = 0;
+    }
+    i++;
+  }
+  return newString;
 }
