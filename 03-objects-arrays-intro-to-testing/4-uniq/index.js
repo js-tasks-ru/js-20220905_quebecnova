589b81f6daf9ae8f0d7d582acc9c8e15b0b2d268
QuebecNova <selllmenya@gmail.com>
2022-09-08 15:28:13 +0300

    done with set

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,11 +4,5 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr = []) {
-  const uniqArr = [];
-  arr.forEach((value) => {
-    if (!uniqArr.includes(value)) {
-      uniqArr.push(value);
-    }
-  });
-  return uniqArr;
+  return [...new Set(arr)];
 }
